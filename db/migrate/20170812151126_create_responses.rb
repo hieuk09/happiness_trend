5796e4d10c602a6ee8576f9c1c8a82589debf981
Hieu Nguyen <hieuk09@gmail.com>
2017-08-12 22:32:00 +0700

    Initial commit

---
+++ db/migrate/20170812151126_create_responses.rb
@@ -0,0 +1,10 @@
+class CreateResponses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :responses do |t|
+      t.references :question
+      t.integer :answer
+
+      t.timestamps
+    end
+  end
+end
